5ac6846984cd5557ab58d85e9b3bc5fcab8e983e
Stevo St. John <stevo.c.stjohn@gmail.com>
2021-04-25 17:33:02 -0500

    updating gitignore to remove 'generated' files, renaming test host

--- Playground/TestHost/ExampleGameEnginePluginHost.cpp
+++ Playground/TestHost/ExampleGameEnginePluginHost.cpp
@@ -1,4 +1,4 @@
-// TestHost.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// ExampleGameEnginePluginHost.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
