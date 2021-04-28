7ef8a3b1a6b063b18833a0ee32d6e9cf7de42ba3
Stevo St. John <stevo.c.stjohn@gmail.com>
2021-04-27 22:25:23 -0500

    adding tests for dynamic load, initialize

--- GameEnginePlugin/ExampleGameEnginePlugin.Tests/test.cpp
+++ GameEnginePlugin/ExampleGameEnginePlugin.Tests/test.cpp
@@ -1,7 +1,34 @@
 #include "pch.h"
-#include "GameEnginePlugin.h"
+#include <Windows.h>
 
-TEST(TestInitializeOK_StaticLoad, TestInitialize) {
-  int initialize_rv = Initialize();
-  EXPECT_EQ(initialize_rv, 0);
+namespace {
+
+    typedef int(__stdcall* Initialize_t)();       // Initialize function signature with calling convention
+
+    class PluginTestFixture : public ::testing::Test {
+    protected:
+        void SetUp() override {
+            hGetProcIDDLL = ::LoadLibrary((LPCWSTR)".\\ExampleGameEnginePlugin.dll");
+            EXPECT_NE(hGetProcIDDLL, (HINSTANCE)0); // library loaded
+        }
+
+        void TearDown() override {
+            BOOL unload_rv = ::FreeLibrary(hGetProcIDDLL);
+            EXPECT_TRUE(unload_rv); // library unloaded
+        }
+
+        HINSTANCE hGetProcIDDLL;
+    };
+
+}
+
+TEST_F(PluginTestFixture, TestInitialize) {
+
+    // resolve function address here
+    Initialize_t initialize_func = (Initialize_t) ::GetProcAddress(hGetProcIDDLL, "Initialize");
+    EXPECT_NE(initialize_func, (Initialize_t) 0); // Initialize function found
+
+    int initialize_rv = initialize_func();
+    EXPECT_EQ(initialize_rv, 0); // initialize ok
+
 }
